2358ef349e609d7dbc0e73774a1ba22272e2c9fd
mahmud035 <mahamudulhasan4148@gmail.com>
2022-11-11 11:09:16 +0600

    70_5-4 Indexing and text search in mongodb

--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -1,17 +1,25 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import ServiceCard from './ServiceCard';
 
 const Services = () => {
   const [services, setServices] = useState([]);
   const [isAscending, setIsAscending] = useState(true);
+  const [search, setSearch] = useState('');
+  const searchRef = useRef();
 
   useEffect(() => {
     fetch(
-      `http://localhost:5000/services?order=${isAscending ? 'asc' : 'desc'}`
+      `http://localhost:5000/services?search=${search}&order=${
+        isAscending ? 'asc' : 'desc'
+      }`
     )
       .then((res) => res.json())
       .then((data) => setServices(data));
-  }, [isAscending]);
+  }, [isAscending, search]);
+
+  const handleSearch = () => {
+    setSearch(searchRef.current.value);
+  };
 
   // console.log(services);
   return (
@@ -24,28 +32,24 @@ const Services = () => {
           distinctio minus sit magnam numquam tempora esse. <br /> Porro
           dignissimos cum nemo id voluptas,
         </p>
+        {/* Mongodb Text Search */}
+        <input
+          ref={searchRef}
+          type="text"
+          placeholder="Type here"
+          className="input input-bordered input-secondary w-full max-w-xs mr-4 mb-8"
+        />
+        <button onClick={handleSearch} className="btn btn-secondary mr-28">
+          Search
+        </button>
 
+        {/* Ascending or Descending Order */}
         <button
           onClick={() => setIsAscending(!isAscending)}
           className="btn btn-accent"
         >
           {isAscending ? 'Descending' : 'Ascending'}
         </button>
-        {/* <input
-          ref={searchRef}
-          type="text"
-          placeholder="Type here"
-          className="input input-bordered input-secondary w-full max-w-xs mb-4 mr-10"
-        />
-        <button onClick={handleSearch} className="btn btn-info mr-16">
-          Search
-        </button>
-        <button
-          onClick={() => setIsAscending(!isAscending)}
-          className="btn btn-accent "
-        >
-          {isAscending ? 'descending' : 'ascending'}
-        </button> */}
       </div>
 
       <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6">
